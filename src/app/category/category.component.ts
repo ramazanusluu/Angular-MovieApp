0ab5fd11673f1d47bfc3fcc34e5b420df1a0a605
Ramazan USLU <ruslu1720@gmail.com>
2022-08-05 23:27:33 +0300

    Bindings

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
 })
 export class CategoryComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
+  categories = ['Macera', 'Romantik', 'Bilim Kurgu', 'Komedi'];
 }
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -3,13 +3,38 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
-  styleUrls: ['./movies.component.css']
+  styleUrls: ['./movies.component.css'],
 })
 export class MoviesComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  //  movies = ['film 1', 'film 2', 'film 3', 'film 4'];
+  movies = [
+    {
+      id: 1,
+      title: 'film 1',
+      description: 'Film 1 açıklama',
+      imageUrl: '1.jpeg',
+    },
+    {
+      id: 2,
+      title: 'film 2',
+      description: 'Film 2 açıklama',
+      imageUrl: '2.jpeg',
+    },
+    {
+      id: 3,
+      title: 'film 3',
+      description: 'Film 3 açıklama',
+      imageUrl: '3.jpeg',
+    },
+    {
+      id: 4,
+      title: 'film 4',
+      description: 'Film 4 açıklama',
+      imageUrl: '4.jpeg',
+    },
+  ];
 }
