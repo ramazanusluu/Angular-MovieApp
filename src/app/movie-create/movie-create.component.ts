4e389a906b4cd5752e937cbb20fd8c05e57043ea
Ramazan USLU <ruslu1720@gmail.com>
2022-08-08 01:26:08 +0300

    Film bilgilerinin kontrol edilmesi - validation

--- src/app/movie-create/movie-create.component.ts
+++ src/app/movie-create/movie-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Category } from '../models/category';
+import { AlertifyService } from '../services/alertify.service';
 import { CategoryService } from '../services/category.service';
 import { MovieService } from '../services/movie.service';
 
@@ -16,7 +17,8 @@ export class MovieCreateComponent implements OnInit {
   constructor(
     private categoryService: CategoryService,
     private movieService: MovieService,
-    private router: Router
+    private router: Router,
+    private alertify: AlertifyService
   ) {}
 
   ngOnInit(): void {
@@ -26,6 +28,33 @@ export class MovieCreateComponent implements OnInit {
   }
 
   createMovie(title: any, description: any, imageUrl: any, categoryId: any) {
+    if (
+      title.value === '' ||
+      description.value === '' ||
+      imageUrl.value === '' ||
+      categoryId.value === '-1'
+    ) {
+      this.alertify.error('Lütfen tüm alanları doldurunuz');
+      return;
+    }
+    if (title.value.length < 5) {
+      this.alertify.error('Title için min 5 karakter girmelisiniz');
+      return;
+    }
+    if (description.value.length < 10 || description.value.length > 50) {
+      this.alertify.error(
+        'Description için min 10 max 50 karakter girmelisiniz'
+      );
+    }
+    const extensions = ['jpeg', 'jpg', 'png'];
+    const extension = imageUrl.value.split('.').pop();
+
+    if (extensions.indexOf(extension) === -1) {
+      this.alertify.error(
+        'Sadece jpeg, jpg, png uzantılı resimler ekleyebilirsiniz'
+      );
+    }
+
     const movie = {
       id: 0,
       title: title.value,
